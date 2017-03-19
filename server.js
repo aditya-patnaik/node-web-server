3ac07c8962dbaef11945f678d2479f9f3b91801e
aditya-patnaik <aditya.narayan.patnaik@gmail.com>
2017-03-19 09:53:00 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials' );
+app.set('view engine', hbs);
+
+//middlewares
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+/*app.use((req, res, next) => {
+	res.render('maintenance.hbs', {
+		pageTitle: 'Maintenance'
+	});
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+//registering template helper
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home',
+		welcomeMessage: 'Hey there!'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000...');
+});
